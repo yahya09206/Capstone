6b301c6d353dc696f585f3ebcdace8d93131bc1c
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-18 17:25:06 -0700

    initialized fields for food collection

--- app/src/main/java/com/yahya/thehorn/ui/FoodActivity.kt
+++ app/src/main/java/com/yahya/thehorn/ui/FoodActivity.kt
@@ -1,11 +1,18 @@
 package com.yahya.thehorn.ui
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
 import com.yahya.thehorn.R
+import com.yahya.thehorn.models.FoodData
 
 class FoodActivity : AppCompatActivity() {
+
+    private val foodsCollectionRef = FirebaseFirestore.getInstance().collection("food")
+    private val foods = mutableListOf<FoodData>()
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
