02a4089beaf336f2dc61f61bef0efd907174827c
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-18 17:30:53 -0700

    set up food fetching list

--- app/src/main/java/com/yahya/thehorn/ui/FoodActivity.kt
+++ app/src/main/java/com/yahya/thehorn/ui/FoodActivity.kt
@@ -4,6 +4,7 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.yahya.thehorn.R
 import com.yahya.thehorn.models.FoodData
@@ -19,6 +20,22 @@ class FoodActivity : AppCompatActivity() {
         setContentView(R.layout.activity_food)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Foods"
+
+        fetchFoods()
+    }
+
+    private fun fetchFoods(){
+        foodsCollectionRef.get().addOnCompleteListener {
+            it.result?.documents?.forEach { snapshot ->
+                foods.add(snapshot.toObject(FoodData::class.java)!!)
+                return@forEach
+            }
+            if(foods.isNotEmpty()){
+                progressBar.visibility = View.GONE
+                contentList.layoutManager = LinearLayoutManager(this)
+                contentList.adapter = FoodsAdapter(this, foods)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?) = super.onOptionsItemSelected(item).also {
@@ -26,4 +43,6 @@ class FoodActivity : AppCompatActivity() {
             android.R.id.home -> finish()
         }
     }
+
+
 }
